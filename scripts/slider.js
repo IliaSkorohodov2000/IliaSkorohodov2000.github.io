640aec1a9ea7fccb43f12aa506644b06daf18007
IliaSkorohodov2000 <64616262+IliaSkorohodov2000@users.noreply.github.com>
2020-05-01 11:43:32 +0300

    Загрузка сайта и отчета

---
+++ scripts/slider.js
@@ -0,0 +1,8 @@
+function first(numb){
+	if(numb === null) numb == 1;
+    setInterval(second(numb), 2000);
+    }
+
+function second(numb){
+	var ourradio = document.getElementById('switch"' + numb + '"').checked=true;
+}
